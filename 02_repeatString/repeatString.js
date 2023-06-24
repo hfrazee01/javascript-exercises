7aa15438a35a8e6557ad92bfb3ab4dfe25ea846e
Harley Frazee <hfrazee01@gmail.com>
2023-06-24 16:50:06 -0400

    Finished second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, repeats) {
+    if (repeats < 0) {
+        return "ERROR";
+    }
 
+    let repeatedString = "";
+    for (let i = 0; i < repeats; i++) {
+        repeatedString += str;
+    }
+
+    return repeatedString;
 };
 
 // Do not edit below this line
