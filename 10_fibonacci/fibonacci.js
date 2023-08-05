648c3ec27c667d59c8fc4c64670f48d21c468bbb
Harley Frazee <hfrazee01@gmail.com>
2023-08-05 18:50:23 -0400

    Finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    let pos = parseInt(position);
 
+    if (pos < 1) return "OOPS";
+    else if (pos == 1 || pos == 2) return 1;
+    else return fibonacci(pos - 1) + fibonacci(pos- 2);
 };
 
 // Do not edit below this line
